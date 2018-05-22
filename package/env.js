6d48ff49321efbdb0389b277ed33ece0bd01d9d7
Ed Slocomb <edslocomb@gmail.com>
2018-05-21 19:31:04 -0700

    read the config/webpack dir for environment names, instead of using a hardcoded list

--- package/env.js
+++ package/env.js
@@ -1,19 +1,23 @@
 const { resolve } = require('path')
 const { safeLoad } = require('js-yaml')
-const { readFileSync } = require('fs')
+const { readdirSync, readFileSync } = require('fs')
 
-const NODE_ENVIRONMENTS = ['development', 'production']
 const DEFAULT = 'production'
-const configPath = resolve('config', 'webpacker.yml')
+const ymlConfigPath = resolve('config', 'webpacker.yml')
 
 const railsEnv = process.env.RAILS_ENV
 const nodeEnv = process.env.NODE_ENV
 
-const config = safeLoad(readFileSync(configPath), 'utf8')
-const availableEnvironments = Object.keys(config).join('|')
-const regex = new RegExp("^(" + availableEnvironments + ")$", 'g')
+const ymlConfig = safeLoad(readFileSync(ymlConfigPath), 'utf8')
+const availableYmlEnvironments = Object.keys(ymlConfig)
+                                       .filter((key) => key !== 'default')
+
+const jsConfigDir = resolve('config', 'webpack')
+const jsConfigs = readdirSync(jsConfigDir)
+  .map((f) => f.replace(/\.[^\.]+$/, ''))
+  .filter((f) => f !== 'environment')
 
 module.exports = {
-  railsEnv: railsEnv && railsEnv.match(regex) ? railsEnv : DEFAULT,
-  nodeEnv: nodeEnv && NODE_ENVIRONMENTS.includes(nodeEnv) ? nodeEnv : DEFAULT
+  railsEnv: railsEnv && availableYmlEnvironments.includes(railsEnv) ? railsEnv : DEFAULT,
+  nodeEnv: nodeEnv && jsConfigs.includes(nodeEnv) ? nodeEnv : DEFAULT
 }
